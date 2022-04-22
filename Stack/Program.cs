321eceebe10898bc43ee421588032d0aa7e7b544
Breno <bcaua205@gmail.com>
2022-04-21 23:09:00 -0300

    Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,10 @@
+﻿using StackInt;
+public class program {
+    static void Main(string[] args)
+    {
+       var s = new Stack(2);
+       s.push(10);
+       s.push(12);  
+       s.print();
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,54 @@
+﻿namespace StackInt;
+
+public class Stack
+{
+    private int[] _st;
+    private int _top;
+    private int _max;
+
+    public Stack(int size)
+    {
+        _max = size;
+        _top = -1;
+        _st = new int[size];
+    }
+
+
+    public void push(int element)
+    {
+        if (_top == _max - 1)
+        {
+            Console.Write("Stack overflow");
+            return;
+        }
+
+        _st[++_top] = element;
+    }
+
+    public int pop()
+    {
+        if (_top == -1)
+        {
+            Console.WriteLine("Stack is empty");
+            return -1;
+        }
+
+        return _st[_top--];
+    }
+
+    public void print()
+    {
+        int i = 0;
+        if (_top == -1)
+        {
+            Console.WriteLine("stack is empty");
+            return;
+        }
+
+        while (i <= _top)
+        {
+            Console.WriteLine($"{_st[i]}");
+            i++;
+        }
+    }
+}
